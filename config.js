80917d5ee1c9469d8c43a802a36304f50018834e
ls <venu@lifesignals.com>
2023-02-04 14:58:53 +0530

    init

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports.config = {
     webhook : "/webhook",
     secret : "LxjVWjH0Qfwbr1n5oj60vuoAUWIvIFXwFHrf29lx9ZqqpWYoNso5fVFUXLfl",
-    pm2_id : "0",
+    pm2_id : 0,
     git_path : "/root/athena_server",
     git_repo : "git@github.com:venusdharan/athena_server.git",
     port:7777
--- index.js
+++ index.js
@@ -5,38 +5,55 @@ const gitPullOrClone = require('git-pull-or-clone')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: config.webhook, secret: config.secret });
 
-http.createServer(function (req, res) {
-    handler(req, res, function (err) {
-      res.statusCode = 404
-      res.end('no such location')
-    })
-}).listen(config.port)
+'use strict';
 
-handler.on('error', function (err) {
-    console.error('Error:', err.message)
-})
-   
-handler.on('push', function (event) {
-    console.log('Received a push event for %s to %s',
-      event.payload.repository.name,
-      event.payload.ref)
+const Hapi = require('@hapi/hapi');
 
-    gitPullOrClone(config.git_repo,config.git_path, (err) => {
-        if (err) return;
-        console.log('SUCCESS!');
-        pm2.connect(function(err) {
+const init = async () => {
 
-            console.log("Restarting process");
-    
-            pm2.restart(config.pm2_id, (err, proc) => {
-                // Disconnects from PM2
-                //pm2.disconnect()
-            })
-    
-        });
-    })
+    const server = Hapi.server({
+        port: config.port,
+        host: '0.0.0.0'
+    });
 
-})
+
+    server.route({
+        method: 'GET',
+        path: config.webhook,
+        handler: function (request, h) {
+            var payload = request.payload;
+            if(payload.pusher.repository.full_name == "venusdharan/athena_server" ){
+                gitPullOrClone(config.git_repo,config.git_path, (err) => {
+                    if (err) return;
+                    console.log('Pulling latest repo');
+                    pm2.connect(function(err) {
+            
+                        console.log("Restarting process");
+                
+                        pm2.restart(config.pm2_id, (err, proc) => {
+                            // Disconnects from PM2
+                            //pm2.disconnect()
+                        })
+                
+                    });
+                })
+            }
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
+
+
 
 console.log("Launching CD CI Tool");
 
